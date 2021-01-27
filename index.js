5b57c17f3e379021313849298db2aebe2b78fb7c
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-01-27 15:39:09 +0100

    date + titre

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const json = require("./movies.json");
-console.log(json);
+//Import fichier JSON sur javascript
+let json = require("./movies.json");
+let listFilm = json[0]
+
+function search(){
+    let date = new Date(listFilm['release_date']*1000);
+    let title = listFilm['title'];
+}
+console.log(title + date);
+
