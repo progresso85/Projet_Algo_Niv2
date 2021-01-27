e6dc36c9e88645a45ca6de5549c04ee9e3afac33
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-01-27 17:02:59 +0100

    story 1 fini

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 //Import fichier JSON sur javascript
 let json = require("./movies.json");
-let listFilm = json[0]
 
-function search(){
-    let date = new Date(listFilm['release_date']*1000);
-    let title = listFilm['title'];
-}
-console.log(title + date);
 
+//Modif date + titre
+function modifTitre(Film){
+    let newdate = new Date(Film['release_date']*1000);
+    let year = newdate.getFullYear();
+    let title = Film['title'];
+    let newTitle = title + "(" + year + ")";
+    console.log(newTitle);
+    Film["title"] = newTitle;
+    console.log(Film);
+};
+
+
+function changeTitle(){
+    for (let i =0; i < json.length; i++){
+        let Film = json[i];
+        modifTitre(Film);
+    }
+};
+changeTitle();
+
+let stringOut = JSON.stringify(json,null,'\t');
+let fs = require('fs');
+fs.writeFile("movies.out.json",stringOut,function(err) {
+    if(err) return console.error(err);
+    console.log('done');
+ });
