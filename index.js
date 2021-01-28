679bdc34e542cc37ecc2bbb85c58afc9ce663d7f
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-01-28 10:57:21 +0100

    trie alpha

--- index.js
+++ index.js
@@ -36,5 +36,26 @@ fs.writeFile("movies.out.json",stringOut,function(err) {
     fs.readFileSync("./movies.json")
     console.timeEnd("sync")
 }
-
 synchronous();
+
+//Import fichier JSON sur javascript
+let json = require("./movies.out.json");
+
+//check tous les titres 
+json.sort(
+    function(a, b){
+    var x = a.title.toLowerCase();
+    var y = b.title.toLowerCase();
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
+    return 0;
+});
+console.log(json);
+
+//Creation d'un nouveau fichier
+let stringOut = JSON.stringify(json,null,'\t');
+let fs = require('fs');
+fs.writeFile("trie.out.json",stringOut,function(err) {
+    if(err) return console.error(err);
+    console.log('done');
+ });
