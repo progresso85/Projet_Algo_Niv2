066f94d618831956911af0b4fe078ab6dd810c00
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-01-28 12:30:57 +0100

    ajout tri alphabetique et croissant

---
+++ changeTitle.js
@@ -0,0 +1,41 @@
+//Import fichier JSON sur javascript
+let json = require("./movies.json");
+
+ 
+//Modif date + titre
+function modifTitre(Film){
+    let newdate = new Date(Film['release_date']*1000);
+    let year = newdate.getFullYear();
+    let title = Film['title'];
+    let newTitle = title + "(" + year + ")";
+    console.log(newTitle);
+    Film["title"] = newTitle;
+    console.log(Film);
+};
+
+//Modif sur tous les films 
+function changeTitle(){
+    for (let i =0; i < json.length; i++){
+        let Film = json[i];
+        modifTitre(Film);
+    }
+};
+changeTitle();
+
+//Creation d'un nouveau fichier
+let stringOut = JSON.stringify(json,null,'\t');
+let fs = require('fs');
+fs.writeFile("movies.out.json",stringOut,function(err) {
+    if(err) return console.error(err);
+    console.log('done');
+ });
+
+ //Temps exe 
+ function synchronous() {
+    console.time("sync");
+    fs.readFileSync("./movies.json")
+    console.timeEnd("sync")
+}
+synchronous();
+
+
